bda10a1cac72386cf471305f68e5efe57177be25
ottodobretsberger <39414503+ottodobretsberger@users.noreply.github.com>
2020-09-14 13:13:45 -0500

    second commit

---
+++ BlazorTechfestServer/Data/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTechfestServer.Data
+{
+    public class SuperHero
+    {
+        public string Name;
+        public int Id;
+    }
+}
